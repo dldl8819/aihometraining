d442c684a288163429f31284220535ccf45ee2e8
pokou125 <94342364+pokou125@users.noreply.github.com>
2022-04-19 04:05:49 +0900

    [LYH] 개인 식단 완성

--- teamproject/src/main/java/aihometraining/team/diet/controller/DietUserController.java
+++ teamproject/src/main/java/aihometraining/team/diet/controller/DietUserController.java
@@ -167,7 +167,6 @@ public class DietUserController {
 		HashMap<String, Object> UserdietPlanListNutrient = dietService.UserdietPlanList(UserdietPlanList);
 		model.addAttribute("UserdietPlanListNutrient", UserdietPlanListNutrient);
 		
-		log.info("나와바ㅗ : {}",UserdietPlanListNutrient);
 		
 		
 		return UserdietPlanListNutrient;
@@ -181,9 +180,47 @@ public class DietUserController {
 		model.addAttribute("dietBankList", dietBankList);
 		
 		
-		log.info("뱅크값:{}",dietBankList);
+		
 		
 		return "diet/AjaxTable/dietMyListSelectBankAjax";
+	}
+	
+	//Ajax user diet에서 식단은행 디테일 불러오기
+	@PostMapping("/selectDietBankListDetail")
+	public String selectDietBankListDetail(Model model,DietBank dietBank) {
+		
+		//select 시간, 요일 넘겨주기
+				List<List<HashMap<String, Object>>> selectBankDay = dietService.selectBankDay();
+				
+				model.addAttribute("Banktime", selectBankDay.get(0));
+				model.addAttribute("Bankday", selectBankDay.get(1));
+				
+		//select 음식 조회, 화면에 뿌려주기
+		String dietBankCode = dietBank.getDietBankCode();
+		DietOnemealConnection dietOnemealConnection = new DietOnemealConnection();
+		dietOnemealConnection.setDietBankCode(dietBankCode);
+		
+		List<HashMap<String, Object>> selectOneMealConn = dietMapper.selectDietOneMealConnectionByBankCode(dietOnemealConnection);
+		model.addAttribute("selectOneMealConn", selectOneMealConn);		
+		
+		
+		
+		return "/diet/AjaxTable/DietMyListSelectBankDetailAjax";
+	}
+	
+	
+	//Ajax user 식단에 dietBank 내용 삽입 
+	@PostMapping("/insertDietBankListDetail")
+	@ResponseBody
+	public String insertDietBankListDetail(DietBank dietBank, HttpSession session) {
+		
+		String memberEmail = (String) session.getAttribute("SEMAIL");
+		dietBank.setMemberEmail(memberEmail);
+		
+		
+		dietService.insertDietOneMealConnectionAll(dietBank);
+		
+		return null;
 	}
 }
 
